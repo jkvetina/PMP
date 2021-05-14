1d0369ae31fcc6ed74f5394ee495b50ef69d1a7f
Jan Kvetina <jan.kvetina@gmail.com>
2021-05-14 21:19:52 +0200

    Grants for new views

--- grants.sql
+++ grants.sql
@@ -5,21 +5,30 @@ GRANT SELECT, UPDATE, INSERT, DELETE ON pmp.skills              TO pmp_apex;
 GRANT SELECT, UPDATE, INSERT, DELETE ON pmp.resources           TO pmp_apex;
 GRANT SELECT, UPDATE, INSERT, DELETE ON pmp.projects            TO pmp_apex;
 --
-GRANT SELECT ON pmp.p200_sprints    TO pmp_apex;
-GRANT SELECT ON pmp.p300_resources  TO pmp_apex;
-GRANT SELECT ON pmp.p300_tasks      TO pmp_apex;
+GRANT SELECT ON pmp.p200_projects               TO pmp_apex;
+GRANT SELECT ON pmp.p200_sprints                TO pmp_apex;
+GRANT SELECT ON pmp.p200_sprints_chart          TO pmp_apex;
+GRANT SELECT ON pmp.p300_resources              TO pmp_apex;
+GRANT SELECT ON pmp.p300_tasks                  TO pmp_apex;
+GRANT SELECT ON pmp.p310_skills                 TO pmp_apex;
 
 
 
-CREATE SYNONYM pmp_apex.tasks               FOR pmp.tasks;
-CREATE SYNONYM pmp_apex.sprints             FOR pmp.sprints;
-CREATE SYNONYM pmp_apex.resource_skills     FOR pmp.resource_skills;
-CREATE SYNONYM pmp_apex.skills              FOR pmp.skills;
-CREATE SYNONYM pmp_apex.resources           FOR pmp.resources;
-CREATE SYNONYM pmp_apex.projects            FOR pmp.projects;
 --
-CREATE SYNONYM pmp_apex.p200_sprints        FOR pmp.p200_sprints;
-CREATE SYNONYM pmp_apex.p300_resources      FOR pmp.p300_resources;
-CREATE SYNONYM pmp_apex.p300_tasks          FOR pmp.p300_tasks;
+-- RUN AS SYS
+--
+CREATE SYNONYM pmp_apex.tasks                   FOR pmp.tasks;
+CREATE SYNONYM pmp_apex.sprints                 FOR pmp.sprints;
+CREATE SYNONYM pmp_apex.resource_skills         FOR pmp.resource_skills;
+CREATE SYNONYM pmp_apex.skills                  FOR pmp.skills;
+CREATE SYNONYM pmp_apex.resources               FOR pmp.resources;
+CREATE SYNONYM pmp_apex.projects                FOR pmp.projects;
+--
+CREATE SYNONYM pmp_apex.p200_projects           FOR pmp.p200_projects;
+CREATE SYNONYM pmp_apex.p200_sprints            FOR pmp.p200_sprints;
+CREATE SYNONYM pmp_apex.p200_sprints_chart      FOR pmp.p200_sprints_chart;
+CREATE SYNONYM pmp_apex.p300_resources          FOR pmp.p300_resources;
+CREATE SYNONYM pmp_apex.p300_tasks              FOR pmp.p300_tasks;
+CREATE SYNONYM pmp_apex.p310_skills             FOR pmp.p310_skills;
 
 
