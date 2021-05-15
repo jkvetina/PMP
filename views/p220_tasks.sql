0f0cf3a5dfb9fcd40fbb5ad3ded9874c8d48b338
Jan Kvetina <jan.kvetina@gmail.com>
2021-05-15 12:21:52 +0200

    Tasks overview (thru Sprints and Projects)

---
+++ views/p220_tasks.sql
@@ -0,0 +1,14 @@
+CREATE OR REPLACE VIEW p220_tasks AS
+SELECT
+    t.*,
+    p.project_name,
+    s.start_at,
+    s.end_at
+FROM tasks t
+JOIN sprints s
+    ON s.sprint_id      = t.sprint_id
+JOIN projects p
+    ON p.project_id     = t.project_id
+WHERE p.is_active       = 'Y'
+    AND s.is_active     = 'Y';
+
