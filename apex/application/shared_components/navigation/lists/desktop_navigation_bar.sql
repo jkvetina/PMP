dbb8170629d58ee77d836c1bf6d3a6fa12154eb3
Jan Kvetina <jan.kvetina@gmail.com>
2021-05-16 13:29:20 +0200

    Removed hierarchy in navigation bar, added link to User Info page

--- apex/application/shared_components/navigation/lists/desktop_navigation_bar.sql
+++ apex/application/shared_components/navigation/lists/desktop_navigation_bar.sql
@@ -17,29 +17,17 @@ wwv_flow_api.create_list(
 ,p_list_status=>'PUBLIC'
 );
 wwv_flow_api.create_list_item(
- p_id=>wwv_flow_api.id(116736755963910611)
+ p_id=>wwv_flow_api.id(118804210430242501)
 ,p_list_item_display_sequence=>10
 ,p_list_item_link_text=>'&APP_USER.'
-,p_list_item_link_target=>'#'
-,p_list_item_icon=>'fa-user'
-,p_list_text_02=>'has-username'
+,p_list_item_link_target=>'f?p=&APP_ID.:800:&APP_SESSION.::&DEBUG.:800'
 ,p_list_item_current_type=>'TARGET_PAGE'
 );
 wwv_flow_api.create_list_item(
- p_id=>wwv_flow_api.id(116737193655910612)
+ p_id=>wwv_flow_api.id(118804629839242502)
 ,p_list_item_display_sequence=>20
-,p_list_item_link_text=>'---'
-,p_list_item_link_target=>'separator'
-,p_parent_list_item_id=>wwv_flow_api.id(116736755963910611)
-,p_list_item_current_type=>'TARGET_PAGE'
-);
-wwv_flow_api.create_list_item(
- p_id=>wwv_flow_api.id(116737562792910614)
-,p_list_item_display_sequence=>30
-,p_list_item_link_text=>'Sign Out'
 ,p_list_item_link_target=>'&LOGOUT_URL.'
 ,p_list_item_icon=>'fa-sign-out'
-,p_parent_list_item_id=>wwv_flow_api.id(116736755963910611)
 ,p_list_item_current_type=>'TARGET_PAGE'
 );
 wwv_flow_api.component_end;
