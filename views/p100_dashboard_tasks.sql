ee7614ba66ecc33f2a3ea877d60df58beb1477b1
Jan Kvetina <jan.kvetina@gmail.com>
2021-05-16 21:48:48 +0200

    First dashboard chart (Tasks and Resources overview)

---
+++ views/p100_dashboard_tasks.sql
@@ -0,0 +1,17 @@
+CREATE OR REPLACE VIEW p100_dashboard_tasks AS
+SELECT
+    p.project_id,
+    p.project_name,
+    --
+    SUM(CASE WHEN t.status = 'COMPLETE' THEN 0 ELSE 1 END)  AS tasks_todo,
+    SUM(CASE WHEN t.status = 'COMPLETE' THEN 1 ELSE 0 END)  AS tasks_done,
+    COUNT(DISTINCT t.resource_id)                           AS resources
+FROM projects p
+JOIN sprints s
+    ON s.project_id     = p.project_id
+    AND s.is_active     = 'Y'
+JOIN tasks t
+    ON t.project_id     = p.project_id
+    AND t.sprint_id     = s.sprint_id
+GROUP BY p.project_id, p.project_name;
+
